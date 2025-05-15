347ff4a9d38657ad4eeb7cb628205aea66cd27ac
Tom Tresansky <ttresansky@gradle.com>
2025-05-15 10:18:17 -0400

    Also test for service directly

--- plugin/src/test/groovy/org/gradle/ai/nlp/plugin/GradleNlpUiPluginTest.groovy
+++ plugin/src/test/groovy/org/gradle/ai/nlp/plugin/GradleNlpUiPluginTest.groovy
@@ -4,12 +4,14 @@ import org.gradle.testfixtures.ProjectBuilder
 import spock.lang.Specification
 
 class GradleNlpUiPluginTest extends Specification {
+    @SuppressWarnings('ConfigurationAvoidance')
     def "plugin registers task"() {
         given:
         def project = ProjectBuilder.builder().build()
         project.plugins.apply("org.gradle.ai.nlp")
 
         expect:
+        project.gradle.getSharedServices().getRegistrations().findByName(GradleNlpUiPlugin.MCP_SERVICE_NAME)
         project.tasks.named(GradleNlpUiPlugin.CUSTOM_TASKS_REPORT_TASK_NAME).get()
         project.tasks.named(GradleNlpUiPlugin.START_MCP_SERVER_TASK_NAME).get()
     }
