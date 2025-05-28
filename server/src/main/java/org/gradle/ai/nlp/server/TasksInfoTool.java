9895842c510eab2f363ced818bf3b265599b8c52
Tom Tresansky <ttresansky@gradle.com>
2025-05-28 19:48:52 -0400

    Better injection for tasksReportFilePath

--- server/src/main/java/org/gradle/ai/nlp/server/TasksInfoTool.java
+++ server/src/main/java/org/gradle/ai/nlp/server/TasksInfoTool.java
@@ -2,27 +2,18 @@ package org.gradle.ai.nlp.server;
 
 import org.springframework.ai.tool.annotation.Tool;
 import com.google.common.io.Files;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.Properties;
 
 @Component
 public final class TasksInfoTool {
-    private Environment environment;
-
-    @Autowired
-    public void setEnvironment(Environment environment) {
-        this.environment = environment;
-    }
+    @Value("${org.gradle.ai.nlp.server.tasks.report.file}")
+    public String tasksReportFilePath;
 
     @Tool(description = "Task report information")
     public String tasksInfoTool() {
-        String tasksReportFilePath = environment.getProperty("org.gradle.ai.nlp.server.tasks.report.file");
         if (null != tasksReportFilePath) {
             try {
                 var tasksReportFile = new java.io.File(tasksReportFilePath);
